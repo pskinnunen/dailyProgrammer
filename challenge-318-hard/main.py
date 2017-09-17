0ba2f8e081de223f6041f8c6c096221968b87119
pskinnunen <pskinnunen@gmail.com>
2017-09-17 17:03:01 -0400

    Finished putting brackets on the subsection.

--- challenge-318-hard/main.py
+++ challenge-318-hard/main.py
@@ -7,10 +7,9 @@ ladder_representation = {
 
 
 
-def generate_logic(ladder_split_input):
+def generate_logic(ladder_split_input, curr_line):
     header_line = 0
     logic_line = 1
-    curr_line = 1
     line_output = ['','']
     #iterator = iter(split_input)
     if('NXB' in ladder_split_input):
@@ -20,23 +19,24 @@ def generate_logic(ladder_split_input):
         subsection_list = ladder_split_input[start_of_subsection:end_of_subsection:]
         if(start_of_subsection != 0):
             del(ladder_split_input[start_of_subsection:end_of_subsection])
-        for sub_index in range(len(subsection_list)):
-            print (subsection_list[sub_index])
     for code_index in range(len(ladder_split_input)):
-        print (ladder_split_input[code_index])
         code = ladder_split_input[code_index]
         if(start_of_subsection != 0 and code_index == start_of_subsection):
             line_output[logic_line] += '-+-'
             line_output[header_line] += '   '
-        if code == 'SOR':
-            line_output[header_line] += '  | '
-            line_output[logic_line] += '{} |-'.format(curr_line)
-        elif code == 'EOR':
-            line_output[logic_line] += '-|'
-            line_output[header_line] += ' |'
+        #if code == 'SOR':
+            #line_output[header_line] += '  | '
+            #line_output[logic_line] += '{} |-'.format(curr_line)
+        if code == 'EOR':
+            line_output[logic_line] = '{} |-'.format(curr_line) + line_output[logic_line] + '-|'
+            line_output[header_line] = '  | ' + line_output[header_line] + ' |'
+            full_line_length = len(line_output[header_line])
+            for line_index in range(2, len(line_output)):
+                line_output[line_index] = '  | ' + line_output[line_index]
+                line_output[line_index] = line_output[line_index].ljust(full_line_length - 1) + '|'
             curr_line += 1
             code_index += 1
-            line_output.extend(generate_logic(ladder_split_input[code_index::]))
+            line_output.extend(generate_logic(ladder_split_input[code_index::], curr_line + 1))
         elif code in ladder_representation:
             line_output[logic_line] +=  ladder_representation[code]
             code_index += 1
@@ -45,10 +45,11 @@ def generate_logic(ladder_split_input):
         elif code == 'BST':
             line_output[logic_line] += '-+-'
             line_output[header_line] += '   '
-            line_output.extend(generate_logic(subsection_list))
-            for line_index in range(2, len(line_output)):
-                print(line_output[line_index])
-                line_output[line_index] = line_output[line_index].rjust(len(line_output[logic_line]) + len(line_output[line_index]) - 2)
+            curr_line_length = len(line_output[logic_line])
+            subsection_output = generate_logic(subsection_list, curr_line)
+            for line_index in range(len(subsection_output)):
+                subsection_output[line_index] = subsection_output[line_index].rjust(curr_line_length + len(line_output[line_index])-1)
+            line_output.extend(subsection_output)
         elif code == 'NXB':
             line_output[logic_line] += '+-'
             line_output[header_line] += '| '
@@ -59,7 +60,7 @@ def generate_logic(ladder_split_input):
 
 def process_ladder_input(ladder_input_string):
     ladder_split_input = ladder_input_string.split()
-    line_output = generate_logic(ladder_split_input)
+    line_output = generate_logic(ladder_split_input, 1)
     for line in line_output:
         print(line)
 
